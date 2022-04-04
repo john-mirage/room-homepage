03e57d85c37a42de4fcfd8ffb3f7455f72f8180c
john-mirage <chaosdynamix@posteo.net>
2022-04-04 05:49:23 +0200

    feat: add reset and some styles

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,32 @@
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/controller';
+import 'swiper/scss/keyboard';
+import 'swiper/scss/effect-fade';
 import './assets/styles/main.scss';
+import Swiper, { Navigation, Keyboard, EffectFade, Controller, A11y } from 'swiper';
+
+const textSwiper = new Swiper("#slider-text", {
+    modules: [EffectFade, A11y],
+    allowTouchMove: false,
+    effect: "fade",
+    fadeEffect: {
+        crossFade: true
+    },
+});
+
+const imageSwiper = new Swiper("#slider-image", {
+    modules: [Navigation, Keyboard, Controller, A11y],
+    resistanceRatio: 0,
+    grabCursor: true,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+    controller: {
+        control: textSwiper,
+    },
+    a11y: {
+        notificationClass: "notification",
+    }
+});
